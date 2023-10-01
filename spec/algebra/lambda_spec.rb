01aeead5c15986d469dfe54db932b47f69f44631
Armin Jöllenbeck <armin@joellenbeck.net>
2023-10-01 23:39:11 +0200

    Auto include spec/spec_helper.rb

--- spec/algebra/lambda_spec.rb
+++ spec/algebra/lambda_spec.rb
@@ -1,5 +1,3 @@
-require_relative '../spec_helper'
-
 module Algebra
   describe Lambda do
     describe '.y_combinator' do
--- spec/algebra/polynomial_spec.rb
+++ spec/algebra/polynomial_spec.rb
@@ -1,5 +1,3 @@
-require_relative '../spec_helper'
-
 module Algebra
   describe Polynomial do
     let :x do
